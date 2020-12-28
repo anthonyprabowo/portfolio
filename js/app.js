b8f279be4e1f0566d6362e0ed3a99009348cd695
aprabowo <aprabowo@github.com>
2020-12-27 23:25:08 -0800

    adding style to module

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
+// Load in particles.js
 particlesJS.load('particles-js', 'assets/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+// slide in effect
+$(window).scroll(function(event) {
+  
+  $(".module").each(function(i, el) {
+    var el = $(el);
+    if (el.visible(true)) {
+      el.addClass("come-in"); 
+      el.removeClass("come-out");
+    } else {
+      el.addClass("come-out");
+      el.removeClass("come-in");
+    }
+  });
+  
+});
